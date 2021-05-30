62a2af688c4a5694bbc8a471ae22a570a6a43c1e
Nanok09 <matyikiraly9@gmail.com>
2021-05-30 18:05:09 +0200

    Nouvelle modif

--- app/src/main/java/com/example/sequence1/ChoixListActivity.kt
+++ app/src/main/java/com/example/sequence1/ChoixListActivity.kt
@@ -1,32 +1,41 @@
 package com.example.sequence1
 
-import android.content.ClipData
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.Gson
+//import com.google.gson.Gson
 
 class ChoixListActivity : AppCompatActivity(), View.OnClickListener {
-
-    var item1 : ItemToDo = ItemToDo("Trop cool", true)
-    var item2 : ItemToDo = ItemToDo("Moins bien", true)
-    var liste1: ListeToDo = ListeToDo("todolist1")
-    var liste2: ListeToDo = ListeToDo("todolist2", mutableListOf(item1, item2))
-
-    var profilListe : ProfilListeToDo = ProfilListeToDo("Macud", mutableListOf(liste1, liste2))
-
-    val profil2 = Gson().fromJson(profilListe.toString(), ProfilListeToDo::class.java)
-
+    private var sp: SharedPreferences? = null
+    private var editor: SharedPreferences.Editor? = null
+//    var item1 : ItemToDo = ItemToDo("Trop cool", true)
+//    var item2 : ItemToDo = ItemToDo("Moins bien", true)
+//    var liste1: ListeToDo = ListeToDo("todolist1")
+//    var liste2: ListeToDo = ListeToDo("todolist2", mutableListOf(item1, item2))
+//
+//    var profilListe : ProfilListeToDo = ProfilListeToDo("Macud", mutableListOf(liste1, liste2))
+//
+//    val profil2 = Gson().fromJson(profilListe.toString(), ProfilListeToDo::class.java)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.choix_list_layout)
+
+        sp = PreferenceManager.getDefaultSharedPreferences(this)
+        val smartCastSp = sp
+        if (smartCastSp != null){
+            editor = smartCastSp.edit()
+        }
+
+
         val bdl = this.intent.extras
-        val chaine_carac = bdl?.getString("string") //pseudo
+        val pseudo = bdl?.getString("string") //pseudo
 
 
         val btnDisp: Button? = findViewById(R.id.displayStuff)
@@ -49,9 +58,17 @@ class ChoixListActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
         when (v.id){
             R.id.displayStuff -> {
-                alerter(profil2.getLogin())
+
             }
         }
+    }
+
+    override fun onStart(){
+        super.onStart()
+        Log.i(CAT, "onStart")
+        val profilListJson = sp!!.getString("login", "login inconnu")
+
+
     }
 
 
