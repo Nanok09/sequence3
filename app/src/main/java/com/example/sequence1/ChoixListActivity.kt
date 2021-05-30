4273c1cdcf75ea420b5bf4cc8bab085fd1e6d903
Nanok09 <matyikiraly9@gmail.com>
2021-05-30 17:34:10 +0200

    Class liste item profil et gson

--- app/src/main/java/com/example/sequence1/ChoixListActivity.kt
+++ app/src/main/java/com/example/sequence1/ChoixListActivity.kt
@@ -1,19 +1,37 @@
 package com.example.sequence1
 
+import android.content.ClipData
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 
-class ChoixListActivity : AppCompatActivity() {
+class ChoixListActivity : AppCompatActivity(), View.OnClickListener {
+
+    var item1 : ItemToDo = ItemToDo("Trop cool", true)
+    var item2 : ItemToDo = ItemToDo("Moins bien", true)
+    var liste1: ListeToDo = ListeToDo("todolist1")
+    var liste2: ListeToDo = ListeToDo("todolist2", mutableListOf(item1, item2))
+
+    var profilListe : ProfilListeToDo = ProfilListeToDo("Macud", mutableListOf(liste1, liste2))
+
+    val profil2 = Gson().fromJson(profilListe.toString(), ProfilListeToDo::class.java)
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.choix_list_layout)
         val bdl = this.intent.extras
-        val chaine_carac = bdl?.getString("string")
+        val chaine_carac = bdl?.getString("string") //pseudo
 
-        alerter(chaine_carac)
+
+        val btnDisp: Button? = findViewById(R.id.displayStuff)
+        btnDisp?.setOnClickListener(this)
+
     }
 
     private fun alerter(s: String?) {
@@ -28,6 +46,13 @@ class ChoixListActivity : AppCompatActivity() {
         private val CAT: String = "EDPMR"
     }
 
+    override fun onClick(v: View) {
+        when (v.id){
+            R.id.displayStuff -> {
+                alerter(profil2.getLogin())
+            }
+        }
+    }
 
 
 }
